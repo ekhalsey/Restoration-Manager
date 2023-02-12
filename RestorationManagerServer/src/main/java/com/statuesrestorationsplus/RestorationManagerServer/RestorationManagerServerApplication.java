c034de9644a6404cdb5008c3d20d804501d0caaf
EmmaHalsey <ekhalsey@yahoo.com>
2023-02-12 10:42:14 -0600

    SpringBoot server created

---
+++ RestorationManagerServer/src/main/java/com/statuesrestorationsplus/RestorationManagerServer/RestorationManagerServerApplication.java
@@ -0,0 +1,13 @@
+package com.statuesrestorationsplus.RestorationManagerServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorationManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorationManagerServerApplication.class, args);
+	}
+
+}
---
+++ RestorationManagerServer/src/test/java/com/statuesrestorationsplus/RestorationManagerServer/RestorationManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statuesrestorationsplus.RestorationManagerServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorationManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
