e99a4975e1d8380017e11c845f0a9b515cdcd103
EmmaHalsey <ekhalsey@yahoo.com>
2023-02-12 09:33:25 -0600

    initial commit spring

---
+++ src/main/java/com/statuesrestorationsplus/RestorationManager/RestorationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.statuesrestorationsplus.RestorationManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestorationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestorationManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/statuesrestorationsplus/RestorationManager/RestorationManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statuesrestorationsplus.RestorationManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestorationManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
