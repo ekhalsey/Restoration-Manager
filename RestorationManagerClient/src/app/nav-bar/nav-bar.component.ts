ca0cb8557aa066afbbf8f64e20ecf36e51ab7660
Nathan-422 <natiouswright@gmail.com>
2023-02-14 17:00:15 -0600

    Basic menu skeleton

--- RestorationManagerClient/src/app/nav-bar/nav-bar.component.ts
+++ RestorationManagerClient/src/app/nav-bar/nav-bar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
+  buttons:String[] = ['Home', 'Clients', 'Projects'];
+
   constructor() { }
 
   ngOnInit(): void {
