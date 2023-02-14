ea847445e3e22877bef3f3036b62d6a76cce9b4d
Nathan-422 <natiouswright@gmail.com>
2023-02-14 16:03:08 -0600

    init nav bar and buttons

--- RestorationManagerClient/src/app/app.module.ts
+++ RestorationManagerClient/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarButtonComponent } from './nav-bar-button/nav-bar-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    NavBarButtonComponent
   ],
   imports: [
     BrowserModule,
