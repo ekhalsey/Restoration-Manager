ef7bac15da0b009dd21a73ee2215578c6e6cb0e2
Nathan-422 <natiouswright@gmail.com>
2023-02-15 10:25:46 -0600

    updated routes

--- RestorationManagerClient/src/app/app-routing.module.ts
+++ RestorationManagerClient/src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddItemComponent } from './add-item/add-item.component';
+import { ClientComponent } from './client/client.component';
 import { HomeComponent } from './home/home.component';
+import { NewClientComponent } from './new-client/new-client.component';
+import { NewProjectComponent } from './new-project/new-project.component';
+import { ProjectComponent } from './project/project.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  {path: 'add', component: AddItem}, 
-  {path: 'project', component: ProjectView},
-  {path: 'client', component: ClientView},
+  { path: 'add', component: AddItemComponent},
+  { path: '/add/client', component: NewClientComponent},
+  { path: '/add/project', component: NewProjectComponent},
+  { path: 'project', component: ProjectComponent},
+  { path: 'client', component: ClientComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
