c7bb3456a5ceafca2d1f791432d5486aea26a3ed
Nathan-422 <natiouswright@gmail.com>
2023-03-07 15:14:41 -0600

    Still working with these

--- RestorationManagerClient/src/app/new-project-form/new-project-form.component.ts
+++ RestorationManagerClient/src/app/new-project-form/new-project-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-new-project-form',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProjectFormComponent implements OnInit {
 
+
+  newProjectForm = new FormGroup({
+    projectName: new FormControl(''),
+    dueDate: new FormControl(''),
+    quotedPrice: new FormControl<number | null>(0),
+    notes: new FormControl(''),
+    photo: new FormControl('')
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  // updateName(): void {
+  //   this.newProjectForm.projectName.setValue("Goobers!");
+  //   this.name.setValue("Goobers!");
+  // }
+
+  onSubmit(): void {
+    console.warn("You are about to submit this form!")
   }
 
 }
