b0f064c1ecaaa0a711fec0a9ea2693e5cf83a259
Nathan-422 <natiouswright@gmail.com>
2023-03-09 16:31:40 -0600

    init new-client-form component

---
+++ RestorationManagerClient/src/app/new-client/new-client-form/new-client-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-client-form',
+  templateUrl: './new-client-form.component.html',
+  styleUrls: ['./new-client-form.component.css']
+})
+export class NewClientFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
